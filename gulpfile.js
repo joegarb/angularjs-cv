5f3535f577c94ea60c1407ac1be31156493d1927
Joe Garber <joegarb@users.noreply.github.com>
2016-08-07 14:55:57 -0400

    Untabify

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,61 @@
 var gulp = require('gulp'),
-		concat = require('gulp-concat'),
-		runSequence = require('run-sequence'),
-		del = require('del'),
-		rename = require('gulp-rename');
+    concat = require('gulp-concat'),
+    runSequence = require('run-sequence'),
+    del = require('del'),
+    rename = require('gulp-rename');
 
 gulp.task('clean', function() {
-	return del(['dist']);
+  return del(['dist']);
 });
 
 gulp.task('build:js', function() {
-	// Bundle JS files
-	// todo: cache busting
+  // Bundle JS files
+  // todo: cache busting
   return gulp.src([
-		'src/js/*.js',
-		'bower_components/angular-css/angular-css.min.js'
-	]).pipe(concat('bundle.js'))
+    'src/js/*.js',
+    'bower_components/angular-css/angular-css.min.js'
+  ]).pipe(concat('bundle.js'))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build:htaccess:prod', function() {
-	// Use the production version of the .htaccess
-	gulp.src('src/.htaccess.production')
-		.pipe(rename('.htaccess'))
-		.pipe(gulp.dest('dist'));
+  // Use the production version of the .htaccess
+  gulp.src('src/.htaccess.production')
+    .pipe(rename('.htaccess'))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build:htaccess', function() {
-	// Just copy the .htaccess since it is already the dev/staging version
-	gulp.src('src/.htaccess')
-		.pipe(gulp.dest('dist'));
+  // Just copy the .htaccess since it is already the dev/staging version
+  gulp.src('src/.htaccess')
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build:static', function() {
-	// Copy static content to the output folder
-	gulp.src([
-		'src/**',
-		// Exclude JS files already bundled and copied
-		'!src/js{,/**}',
-	]).pipe(gulp.dest('dist'));
+  // Copy static content to the output folder
+  gulp.src([
+    'src/**',
+    // Exclude JS files already bundled and copied
+    '!src/js{,/**}',
+  ]).pipe(gulp.dest('dist'));
 });
 
 gulp.task('build:prod', (done) => {
-	runSequence(
-		'clean',
-		'build:js',
-		'build:htaccess:prod',
-		'build:static',
-	done)
+  runSequence(
+    'clean',
+    'build:js',
+    'build:htaccess:prod',
+    'build:static',
+  done)
 });
 
 gulp.task('build', (done) => {
-	runSequence(
-		'clean',
-		'build:js',
-		'build:htaccess',
-		'build:static',
-	done)
+  runSequence(
+    'clean',
+    'build:js',
+    'build:htaccess',
+    'build:static',
+  done)
 });
 
 gulp.task('default', ['build']);
