ebad9950a7ab394ec5de92f1e127b1268f9616d1
Joe Garber <joegarb@users.noreply.github.com>
2017-03-27 21:37:20 -0400

    Fix lint issues

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 'use strict';
 
-/* eslint max-len: "off" */
-
 require('angular').module(
   'joegarb',
-  ['ngRoute', 'angularCSS', 'joegarb.controllers', 'joegarb.directives', 'joegarb.filters'],
+  [
+    'ngRoute', 'angularCSS',
+    'joegarb.controllers', 'joegarb.directives', 'joegarb.filters'
+  ],
   ['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-    $routeProvider.when(
-      '/resume',
-      {
-        templateUrl: 'components/resume/resume.html',
-        controller: 'ResumeController',
-        css: 'components/resume/resume.css'
-      }
-    );
+    function($routeProvider, $locationProvider) {
+      $routeProvider.when(
+        '/resume',
+        {
+          templateUrl: 'components/resume/resume.html',
+          controller: 'ResumeController',
+          css: 'components/resume/resume.css'
+        }
+      );
 
-    // Redirect all requests to the resume page
-    $routeProvider.otherwise(
-      {
-        redirectTo: '/resume'
-      }
-    );
+      // Redirect all requests to the resume page
+      $routeProvider.otherwise(
+        {
+          redirectTo: '/resume'
+        }
+      );
 
-    $locationProvider.html5Mode(true);
-  }]
+      $locationProvider.html5Mode(true);
+    }]
 );
 
 require('angular-route');
