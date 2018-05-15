9057c58e65521d3fbbce0b9d5003b023e69a9bcf
Joe Garber <joegarb@users.noreply.github.com>
2018-05-14 23:29:30 -0400

    Clean up old revisioned files in the build folder, and add cache headers

--- gulp/build.js
+++ gulp/build.js
@@ -6,14 +6,20 @@ const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
-const inlineSource = require('gulp-inline-source');
-const gulpif = require('gulp-if');
-const rev = require('gulp-rev');
-const revReplace = require('gulp-rev-replace');
 const sass = require('gulp-sass');
+const inlineSource = require('gulp-inline-source');
+const filter = require('gulp-filter');
+const rev = require('gulp-rev');
+const revFormat = require('gulp-rev-format');
+const revRewrite = require('gulp-rev-rewrite');
+const revOutdated = require('gulp-rev-outdated');
+const rimraf = require('rimraf');
+const path = require('path');
+const through = require('through2');
 const log = require('fancy-log');
+const PluginError = require('plugin-error');
 
-gulp.task('build', ['clean', 'lint', 'setup'], () => {
+gulp.task('build', ['setup'], () => {
     return Promise.all([
         new Promise((resolve, reject) => {
             log('Building javascript');
@@ -64,22 +70,58 @@ gulp.task('build', ['clean', 'lint', 'setup'], () => {
         return new Promise((resolve, reject) => {
             log('Inlining js/css/images into the html');
             gulp
-                .src('dist/*.html')
-                .pipe(inlineSource())
+                .src('dist/**/*.html')
+                .pipe(inlineSource({rootpath: path.resolve('dist')}))
                 .pipe(gulp.dest('dist'))
                 .on('end', () => {
-                    log('Versioning filenames for cache busting');
+
+                    log('Cleaning old versioned files');
                     gulp
                         .src([
                             'dist/**/*.html',
                             'dist/**/*.css',
                             'dist/**/*.js',
-                            'dist/**/*.{jpg,png,jpeg,gif,svg}'])
-                        .pipe(gulpif('!index.html', rev())) // Rename files except for index.html
-                        .pipe(revReplace()) // Replace within each file any references to files that got renamed
-                        .pipe(gulp.dest('dist'))
-                        .on('end', resolve);
+                            'dist/**/*.{jpg,png,jpeg,gif,svg}'
+                        ], {read: false})
+                        .pipe(revOutdated(10)) // Keep the latest 10 versions of each file just to be super safe if people have index.html cached and referencing them
+                        .pipe(cleaner())
+                        .on('finish', () => {
+
+                            log('Versioning modified files for cache busting');
+
+                            const indexHtmlFilter = filter(['**/*', '!**/index.html'], {restore: true});
+                            const revFilter = filter(['**/*', '!**/*-rev-*'], {restore: true});
+
+                            gulp
+                                .src([
+                                    'dist/**/*.html',
+                                    'dist/**/*.css',
+                                    'dist/**/*.js',
+                                    'dist/**/*.{jpg,png,jpeg,gif,svg}'
+                                ])
+                                .pipe(indexHtmlFilter)
+                                .pipe(revFilter)
+                                .pipe(rev()) // Rename modified files
+                                .pipe(revFormat({prefix: '-rev-'}))
+                                .pipe(revFilter.restore)
+                                .pipe(indexHtmlFilter.restore)
+                                .pipe(revRewrite()) // Replace within each file any references to files that got renamed
+                                .pipe(gulp.dest('dist'))
+                                .on('end', resolve);
+                        });
                 });
         });
     });
 });
+
+function cleaner() {
+    return through.obj(function(file, enc, cb) {
+        rimraf(path.resolve((file.cwd || process.cwd()), file.path), function(err) {
+            if (err) {
+                this.emit('error', new PluginError('Cleanup old files', err));
+            }
+            this.push(file);
+            cb();
+        }.bind(this));
+    });
+}
